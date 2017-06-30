d58dda2c39767106a4bcaa13746b25ab778cea01
kiwidog <forums@allenthinks.com>
2017-06-30 19:33:39 -0400

    Added a bare template for Bundle. This will need to be implemented, as well as the Dbx, Ebx, Resource, Chunk objects that are within a bundle.

---
+++ Loader2013_2/Frostbite/Bundles/Bundle.cs
@@ -0,0 +1,223 @@
+﻿using RimeLib.Frostbite.Bundles;
+using RimeLib.Frostbite.Db;
+using RimeLib.IO;
+using System;
+
+namespace Loader2013_2.Frostbite.Bundles
+{
+    public class Bundle : BundleBase
+    {
+        /// <summary>
+        /// Magic Salt
+        /// </summary>
+        public int MagicSalt { get; set; }
+
+        /// <summary>
+        /// To align the members or not
+        /// </summary>
+        public bool AlignMembers { get; set; }
+
+        /// <summary>
+        /// Total size of this bundle
+        /// </summary>
+        public long TotalSize { get; set; }
+
+        /// <summary>
+        /// Bundle constructor
+        /// </summary>
+        /// <param name="p_Object">DbObject containing the information about this bundle</param>
+        /// <param name="p_Entry"></param>
+        public Bundle(DbObject p_Object, BundleEntry p_Entry)
+        {
+            Entry = p_Entry;
+
+            Path = (string)p_Object["path"].Value;
+
+            MagicSalt = (int)p_Object["magicSalt"].Value;
+            AlignMembers = (bool)p_Object["alignMembers"].Value;
+            TotalSize = (long)p_Object["totalSize"].Value;
+
+            // Load all of the resource objects
+            if (p_Object["res"] != null)
+            {
+                var s_Resources = p_Object["res"].Value as DbObject;
+
+                if (s_Resources != null)
+                    for (var i = 0; i < s_Resources.Count; ++i)
+                        throw new NotImplementedException(); // TODO: Implement Resource
+                        //m_Resources.Add(new Resource(s_Resources[i].Value as DbObject, this));
+            }
+
+            // Load all of the dbx objects
+            if (p_Object["dbx"] != null)
+            {
+                var s_Dbx = p_Object["dbx"].Value as DbObject;
+
+                if (s_Dbx != null)
+                    for (var i = 0; i < s_Dbx.Count; ++i)
+                        throw new NotImplementedException(); // TODO: Implement Dbx
+                        //m_DBX.Add(new Dbx(s_Dbx[i].Value as DbObject, this));
+            }
+
+            // Load all of the ebx objects
+            if (p_Object["ebx"] != null)
+            {
+                var s_Ebx = p_Object["ebx"].Value as DbObject;
+
+                if (s_Ebx != null)
+                    for (var i = 0; i < s_Ebx.Count; ++i)
+                        throw new NotImplementedException(); // TODO: Implement 
+                        //m_EBX.Add(new EBX(s_Ebx[i].Value as DbObject, this));
+            }
+
+            var s_ChunkMeta = p_Object["chunkMeta"] != null ? p_Object["chunkMeta"].Value as DbObject : null;
+
+            // Load all of the chunk objects
+            if (p_Object["chunks"] != null)
+            {
+                var s_Chunks = p_Object["chunks"].Value as DbObject;
+
+                if (s_Chunks != null)
+                {
+                    for (var i = 0; i < s_Chunks.Count; ++i)
+                    {
+                        throw new NotImplementedException(); // TODO: Implement
+                        //var s_Chunk = new Chunk(s_Chunks[i].Value as DbObject, this);
+
+                        //if (s_ChunkMeta != null)
+                        //    s_Chunk.Meta = s_ChunkMeta[i].Value as DbObject;
+
+                        //m_Chunks.Add(s_Chunk);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Bundle constructor from BundleManifest
+        /// </summary>
+        /// <param name="p_Manifest">Bundle manifest</param>
+        /// <param name="p_Reader">Opened reader to the start to the</param>
+        /// <param name="p_Entry">Bundle Entry Information</param>
+        public Bundle(BundleManifest p_Manifest, RimeReader p_Reader, BundleEntry p_Entry)
+        {
+            Entry = p_Entry;
+            Path = p_Entry.ID;
+
+            if (p_Manifest.RealManifest.EbxMode)
+                ParseEbxBundle(p_Manifest, p_Reader);
+            else
+                ParseDbxBundle(p_Manifest, p_Reader);
+        }
+
+        /// <summary>
+        /// Align a reader to 16 byte alignment
+        /// </summary>
+        /// <param name="p_Reader"></param>
+        private void Align16(RimeReader p_Reader)
+        {
+            if (p_Reader.Position % 16 == 0)
+                return;
+
+            var s_Number = 16 - (p_Reader.Position % 16);
+            p_Reader.ReadBytes((int)s_Number);
+        }
+
+        /// <summary>
+        /// Parses an ebx bundle
+        /// </summary>
+        /// <param name="p_Manifest">Bundle Manifest</param>
+        /// <param name="p_Reader">Reader opened to the position of the bundle</param>
+        protected void ParseEbxBundle(BundleManifest p_Manifest, RimeReader p_Reader)
+        {
+            // TODO: Implement VeniceBundleManifest
+            throw new NotImplementedException();
+            //var s_Manifest = p_Manifest.RealManifest as VeniceBundleManifest;
+
+            //// Align to 16 because of the header.
+            //Align16(p_Reader);
+
+            //var s_TextBlockReader = new RimeReader(new MemoryStream(s_Manifest.TextBlock), Endianness.BigEndian);
+
+            //// Start reading entries.
+            //for (var i = 0; i < p_Manifest.ManifestHeader.DbxCount; ++i)
+            //{
+            //    var s_Entry = s_Manifest.Records[i];
+
+            //    // Read the name of the entry.
+            //    s_TextBlockReader.Seek(s_Entry.NameOffset, SeekOrigin.Begin);
+            //    var s_Name = s_TextBlockReader.ReadNullTerminatedString();
+
+            //    // Get the SHA1 hash.
+            //    var s_Hash = s_Manifest.HashBase[i];
+
+            //    // Create the EBX entry.
+            //    var s_Ebx = new EBX(s_Name, s_Hash, s_Entry, p_Reader.ReadBytes((int)s_Manifest.Records[i].PayloadSize), this);
+
+            //    m_EBX.Add(s_Ebx);
+
+            //    Align16(p_Reader);
+            //}
+
+            //// Read resource entries.
+            //for (var i = (int)p_Manifest.ManifestHeader.DbxCount; i < p_Manifest.ManifestHeader.DbxCount + p_Manifest.ManifestHeader.ResourceCount; ++i)
+            //{
+            //    var s_Entry = s_Manifest.Records[i];
+
+            //    // Read the name of the entry.
+            //    s_TextBlockReader.Seek(s_Entry.NameOffset, SeekOrigin.Begin);
+            //    var s_Name = s_TextBlockReader.ReadNullTerminatedString();
+
+            //    // Get the SHA1 hash.
+            //    var s_Hash = s_Manifest.HashBase[i];
+
+            //    // Get the Type hash.
+            //    var s_TypeHash = s_Manifest.ResourceTypeHash[(int)(i - p_Manifest.ManifestHeader.DbxCount)];
+
+            //    // Get the Meta block.
+            //    var s_Meta = s_Manifest.ResourceMeta[(int)(i - p_Manifest.ManifestHeader.DbxCount)];
+
+            //    // Create the Resource entry.
+            //    var s_Resource = new Resource(s_Name, s_Hash, s_TypeHash, p_Reader.ReadBytes((int)s_Manifest.Records[i].PayloadSize), s_Meta, s_Entry, this);
+
+            //    m_Resources.Add(s_Resource);
+
+            //    Align16(p_Reader);
+            //}
+
+            //// Read chunk entries.
+            //var s_ChunkMeta = s_Manifest.ChunkMeta != null && s_Manifest.ChunkMeta["chunkMeta"] != null ? s_Manifest.ChunkMeta["chunkMeta"].Value as DbObject : null;
+
+            //for (var i = 0; i < p_Manifest.ManifestHeader.ChunkCount; ++i)
+            //{
+            //    var s_Entry = s_Manifest.Chunks[i];
+
+            //    // Get the SHA1 hash.
+            //    var s_Hash = s_Manifest.HashBase[i + (int)(p_Manifest.ManifestHeader.DbxCount + p_Manifest.ManifestHeader.ResourceCount)];
+
+            //    // Get the Meta block.
+            //    var s_Meta = s_ChunkMeta != null ? s_ChunkMeta[i].Value as DbObject : null;
+
+            //    // Create the Chunk entry.
+            //    var s_Data = p_Reader.ReadBytes((int)(s_Manifest.Chunks[i].RangeEnd - s_Manifest.Chunks[i].RangeStart));
+            //    var s_Chunk = new Chunk(s_Hash, s_Meta, s_Entry, s_Data, this);
+
+            //    m_Chunks.Add(s_Chunk);
+
+            //    Align16(p_Reader);
+            //}
+
+            //s_TextBlockReader.Dispose();
+        }
+
+        /// <summary>
+        /// Parse a dbx bundle
+        /// </summary>
+        /// <param name="p_Manifest">Bundle manifest</param>
+        /// <param name="p_Reader">Reader opened to the dbx file</param>
+        protected void ParseDbxBundle(BundleManifest p_Manifest, RimeReader p_Reader)
+        {
+            throw new NotImplementedException("We currently do not support DBX-mode bundles.");
+        }
+    }
+}
--- Loader2013_2/Frostbite/Bundles/BundleManager.cs
+++ Loader2013_2/Frostbite/Bundles/BundleManager.cs
@@ -4,23 +4,47 @@ using System.Collections.Generic;
 using RimeLib.Frostbite.Bundles;
 using RimeCommon.VFS.Backends;
 using RimeCommon.VFS;
+using System.Collections.Concurrent;
+using RimeLib.IO;
+using RimeCommon.Messaging;
+using RimeCommon.Logging;
+using RimeLib.IO.Conversion;
+using RimeCommon.Messages.Mounting;
+using RimeCommon.Logging.Messages;
+using System.IO;
+using RimeLib.Frostbite.Db;
 
 namespace Loader2013_2.Frostbite.Bundles
 {
     /// <summary>
     /// Bundle manager for Frostbite 2013.2 engine bundles
     /// </summary>
-    class BundleManager : BaseBundleManager
+    public class BundleManager : BaseBundleManager
     {
+        /// <summary>
+        /// This is an internal structure for tracking purposes
+        /// </summary>
+        protected struct InternalBundleEntry
+        {
+            public BundleEntry BundleEntry { get; set; }
+            public SuperbundleBase SuperbundleBase { get; set; }
+            public bool Mounted { get; set; }
+        }
+
+        private readonly ConcurrentDictionary<string, InternalBundleEntry> m_Bundles;
+
         /// <summary>
         /// Constructor that takes a parent plugin
         /// </summary>
         /// <param name="p_Plugin">Loader2013_2Plugin</param>
         public BundleManager(Loader2013_2Plugin p_Plugin) : base(p_Plugin)
         {
-            // TODO: Create a collection of bundle's
-            // VFS
+            m_Bundles = new ConcurrentDictionary<string, InternalBundleEntry>();
+
+            // Mount bundles, resources, and ebx to the VFS
             FileSystem.Mount(new BundleBackend(), "/bundles");
+            FileSystem.Mount(new ResourceBackend(), "/res");
+            FileSystem.Mount(new EbxBackend(), "/ebx");
         }
 
         /// <summary>
@@ -29,9 +53,16 @@ namespace Loader2013_2.Frostbite.Bundles
         /// <param name="p_Superbundle"></param>
         public override void DiscoverBundles(SuperbundleBase p_Superbundle)
         {
-            foreach (var l_Bundle in p_Superbundle.BundleEntries)
+            foreach (var s_Bundle in p_Superbundle.BundleEntries)
             {
-                // TODO: Add each bundle entry to a collection
+                var s_Entry = new InternalBundleEntry
+                {
+                    BundleEntry = s_Bundle.Value,
+                    SuperbundleBase = p_Superbundle,
+                    Mounted = false
+                };
+
+                m_Bundles.AddOrUpdate(s_Bundle.Key, s_Entry, (k, v) => s_Entry);
             }
         }
 
@@ -42,8 +73,11 @@ namespace Loader2013_2.Frostbite.Bundles
         /// <returns>True on success, false otherwise</returns>
         public override bool DismountBundle(string p_Name)
         {
-            // TODO: Remove the bundle from the collection
-            throw new NotImplementedException();
+            // TODO: Dismount bundle from VFS
+            InternalBundleEntry s_Entry;
+            m_Bundles.TryRemove(p_Name, out s_Entry);
+
+            return true;
         }
 
         /// <summary>
@@ -51,8 +85,8 @@ namespace Loader2013_2.Frostbite.Bundles
         /// </summary>
         public override void DismountBundles()
         {
-            // TODO: Remove all bundles from the collection
-            throw new NotImplementedException();
+            // TODO: Dismount bundles from VFS
+            m_Bundles.Clear();
         }
 
         /// <summary>
@@ -61,7 +95,7 @@ namespace Loader2013_2.Frostbite.Bundles
         /// <returns>List<string> of the bundle full names</returns>
         public override List<string> GetDiscoveredBundles()
         {
-            throw new NotImplementedException();
+            return new List<string>(m_Bundles.Keys);
         }
 
         /// <summary>
@@ -80,7 +114,149 @@ namespace Loader2013_2.Frostbite.Bundles
         /// <param name="p_Superbundle">Superbundle to load all bundles that are contained within</param>
         public override void MountBundles(SuperbundleBase p_Superbundle)
         {
-            throw new NotImplementedException();
+            var s_Endianness = p_Superbundle.BaseLayout.Cas ? Endianness.LittleEndian : Endianness.BigEndian;
+
+            RimeReader s_BaseReader;
+            if (!FileSystem.OpenFileRead(p_Superbundle.BasePath + ".sb", out s_BaseReader, s_Endianness))
+                return;
+
+            RimeReader s_PatchReader = null;
+
+            if (p_Superbundle.PatchPath != null &&
+                !FileSystem.OpenFileRead(p_Superbundle.PatchPath + ".sb", out s_PatchReader, s_Endianness))
+            {
+                s_BaseReader.Dispose();
+                return;
+            }
+
+            foreach (var s_Bundle in p_Superbundle.BundleEntries)
+            {
+                MessageManager.SendMessageAsync(new LogWriteMessage
+                {
+                    LogLevel = LogsLevel.Info,
+                    Message = $"Mounting superbundle '{p_Superbundle.Name}' bundle [{s_Bundle.Key}]..."
+                });
+
+                BundleBase s_MountedBundle;
+
+                if (p_Superbundle.AuthoritativeLayout != null && s_Bundle.Value.BaseSuperbundle.Cas)
+                    s_MountedBundle = MountPatchedCASBundle(p_Superbundle, s_Bundle.Value, s_BaseReader, s_PatchReader);
+                else if (s_Bundle.Value.DeltaEntry != null && s_Bundle.Value.DeltaEntry.Delta)
+                    s_MountedBundle = MountPatchedDeltaBundle(p_Superbundle, s_Bundle.Value, s_BaseReader, s_PatchReader);
+                else if (s_Bundle.Value.DeltaEntry != null)
+                    s_MountedBundle = MountPatchedBundle(p_Superbundle, s_Bundle.Value, s_BaseReader, s_PatchReader);
+                else if (s_Bundle.Value.BaseSuperbundle.Cas)
+                    s_MountedBundle = MountCASBundle(p_Superbundle, s_Bundle.Value, s_BaseReader, s_PatchReader);
+                else
+                    s_MountedBundle = MountBundle(p_Superbundle, s_Bundle.Value, s_BaseReader, s_PatchReader);
+
+                // VFS
+                if (s_MountedBundle == null)
+                    return;
+
+                MountBundleToVFS(s_MountedBundle);
+            }
+
+            s_BaseReader.Dispose();
+
+            s_PatchReader?.Dispose();
+
+            MessageManager.SendMessage(new SuperbundleBundlesMountedMessage { SuperbundleBase = p_Superbundle });
+
+            MessageManager.SendMessageAsync(new LogWriteMessage
+            {
+                LogLevel = LogsLevel.Info,
+                Message = $"Finished mounting superbundle '{p_Superbundle.Name}' bundles!"
+            });
+        }
+
+        /// <summary>
+        /// Mounts a content addressable storage bundle
+        /// </summary>
+        /// <param name="p_Superbundle">Superbundle to get bundles from</param>
+        /// <param name="p_BundleEntry">The bundle entry information</param>
+        /// <param name="p_BaseReader">The reader of the base file</param>
+        /// <param name="p_PatchReader">The reader of the patch file</param>
+        /// <returns>Loaded BundleBase object</returns>
+        private BundleBase MountCASBundle(SuperbundleBase p_Superbundle, BundleEntry p_BundleEntry, RimeReader p_BaseReader, RimeReader p_PatchReader)
+        {
+            p_BaseReader.Seek(p_BundleEntry.Offset, SeekOrigin.Begin);
+
+            var s_Object = new DbObject(p_BaseReader, p_BundleEntry.Size);
+            var s_Bundle = new Bundle(s_Object[0].Value as DbObject, p_BundleEntry);
+
+            p_Superbundle.Bundles.TryAdd(p_BundleEntry.ID, s_Bundle);
+
+            return s_Bundle;
+        }
+
+        private BundleBase MountPatchedCASBundle(SuperbundleBase p_Superbundle, BundleEntry p_BundleEntry, RimeReader p_BaseReader, RimeReader p_PatchReader)
+        {
+            throw new NotImplementedException("Patch files for Frostbite 2013.2 not supported.");
+        }
+
+        /// <summary>
+        /// Helper function to mount a base bundle
+        /// </summary>
+        /// <param name="p_Superbundle">Superbundle to get bundles from</param>
+        /// <param name="p_BundleEntry">The bundle entry information</param>
+        /// <param name="p_BaseReader">The reader of the base file</param>
+        /// <param name="p_PatchReader">The reader of the patch file</param>
+        /// <returns>Loaded BundleBase object</returns>
+        private BundleBase MountBundle(SuperbundleBase p_Superbundle, BundleEntry p_BundleEntry, RimeReader p_BaseReader, RimeReader p_PatchReader)
+        {
+            p_BaseReader.Seek(p_BundleEntry.Offset, SeekOrigin.Begin);
+
+            var s_Manifest = new BundleManifest(p_BaseReader, p_BundleEntry);
+            var s_Bundle = s_Manifest.RealManifest.Bundle;
+
+            p_Superbundle.Bundles.TryAdd(p_BundleEntry.ID, s_Bundle);
+
+            return s_Bundle;
+        }
+
+        /// <summary>
+        /// Helper function to mount a patched bundle
+        /// </summary>
+        /// <param name="p_Superbundle">Superbundle to get bundles from</param>
+        /// <param name="p_BundleEntry">The bundle entry information</param>
+        /// <param name="p_BaseReader">The reader of the base file</param>
+        /// <param name="p_PatchReader">The reader of the patch file</param>
+        /// <returns>BundleBase object</returns>
+        private BundleBase MountPatchedBundle(SuperbundleBase p_Superbundle, BundleEntry p_BundleEntry, RimeReader p_BaseReader, RimeReader p_PatchReader)
+        {
+            throw new NotImplementedException("Patch files for Frostbite 2013.2 not supported.");
+        }
+
+        /// <summary>
+        /// Helper function to mount patched bundles
+        /// </summary>
+        /// <param name="p_Superbundle">Superbundle to get bundles from</param>
+        /// <param name="p_BundleEntry">The bundle entry information</param>
+        /// <param name="p_BaseReader">The reader of the base file</param>
+        /// <param name="p_PatchReader">The reader of the patch file</param>
+        /// <returns>BundleBase object</returns>
+        private BundleBase MountPatchedDeltaBundle(SuperbundleBase p_Superbundle, BundleEntry p_BundleEntry, RimeReader p_BaseReader, RimeReader p_PatchReader)
+        {
+            throw new NotImplementedException("Patch files for Frostbite 2013.2 not supported.");
+        }
+
+        /// <summary>
+        /// This will parse a loaded bundle and mount everything to the VFS
+        /// </summary>
+        /// <param name="p_Bundle">Bundle to be mounted</param>
+        private void MountBundleToVFS(BundleBase p_Bundle)
+        {
+            FileSystem.Mount("/bundles", p_Bundle);
+
+            foreach (var s_Chunk in p_Bundle.ChunkEntries)
+                FileSystem.Mount("/chunks", s_Chunk);
+
+            foreach (var s_Resource in p_Bundle.ResourceEntries)
+                FileSystem.Mount("/res", s_Resource);
+
+            foreach (var s_Ebx in p_Bundle.PartitionEntries)
+                FileSystem.Mount("/ebx", s_Ebx);
         }
     }
 }
